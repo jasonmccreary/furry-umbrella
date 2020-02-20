7a31b066736d74e24070bb602cad9e595fd4c2a7
Jason McCreary <jason@pureconcepts.net>
2020-02-20 16:16:29 +0000

    Make posts soft deletable

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['author', 'title', 'content'];
 }
